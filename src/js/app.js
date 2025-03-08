4b20ae7b25021904a3898a0213362bf05e3ac05b
zsline <sergei.dereka@gmail.com>
2025-03-08 18:05:00 +0200

    08-08-18

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,24 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 
 flsFunctions.isWebp();
 
-const swiper = new Swiper();
+// const swiper = new Swiper();
+
+const header = document.querySelector('.header-page');
+const headerHeight = header.offsetHeight;
+const main = document.querySelector('.main');
+
+if(header){
+    window.addEventListener('scroll', () => {
+        let scrollDistance = window.scrollY;
+        if(scrollDistance >= 600 + headerHeight){
+            header.classList.add('header--fixed');
+            main.style.marginTop = `${headerHeight}px`;
+        } else {
+            header.classList.remove('header--fixed');
+            main.style.marginTop = 0 + `px`;
+        }         
+    })
+}
 
 
 
